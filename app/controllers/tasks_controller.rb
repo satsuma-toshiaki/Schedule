48b6921716e135a97352b63cd78fdf28a101e049
satsuma-toshiaki <satsuma-toshiaki@outlook.jp>
2016-09-01 17:22:18 +0900

    検索機能の向上

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -62,8 +62,16 @@ class TasksController < ApplicationController
   end
 
   def search
-    @tasks = Task.where("name like '%#{params["search"]["name"]}%'")
-    @word = params["search"]["name"] 
+    @search_value = params["search"]["name"] 
+    @search_user = params["search"]["user"] 
+    @search_category = params["search"]["category"] 
+    @tasks = Task.where("name like '%#{@search_value}%'")
+    if @search_user.present?
+      @tasks = @tasks.where(user_id: @search_user)
+    end
+    if @search_category.present?
+      @tasks = @tasks.where(category_id: @search_category)
+    end
     render :index
   end
 
